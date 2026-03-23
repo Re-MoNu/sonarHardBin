99d25a2ffa63d73a9ee85d96c93aa9aef4950d37
Zackary Uhm <78672384+Re-MoNu@users.noreply.github.com>
2026-03-22 21:03:06 -0500

    added test module

---
+++ testmain.py
@@ -0,0 +1,29 @@
+from sonarBins import sonarBins
+import random
+
+random.seed()
+def move_servo(angle):
+    pass
+def read_ultrasonic():
+    if scanner.current_angle > 150 and scanner.current_angle < 160:
+        return random.randint(150,200)
+    return random.randint(90,100)
+
+def on_alert(angle):
+    print("Alert at ", angle)
+
+scanner = sonarBins(
+    move=move_servo,          # required: move(angle)
+    read=read_ultrasonic,     # required: returns distance or None
+    alert=on_alert,           # required: alert(angle)
+    bins=60,                  # default
+    step=1,                   # default
+    start_angle=0,            # default
+    end_angle=180,            # default
+    error_margin=0.15,        # default (15% change per reading)
+    error_ratio=0.6,          # default (60% of readings must fail to call alert())
+    debug=True               # default
+)
+
+scanner.initialize(3)
+print(scanner.sweep())
